f983c9bc843acf62d9b67de93cf635cb43653d55
gabosarmiento <gabrielsarmiento@gmail.com>
2015-11-27 14:58:31 -0500

    Initial Commit

---
+++ Alcolator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alcolator
+//
+//  Created by gabosarmiento on 27/11/15.
+//  Copyright (c) 2015 gabosarmiento. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by gabosarmiento on 27/11/15.
+//  Copyright (c) 2015 gabosarmiento. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
