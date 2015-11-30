8293b565560f7698af507b98ff42e906e0e36149
gabosarmiento <gabrielsarmiento@gmail.com>
2015-11-29 21:25:54 -0500

    ready

--- Alcolator/ViewController.h
+++ Alcolator/ViewController.h
@@ -1,15 +1,17 @@
 //
 //  ViewController.h
-//  Alcolator
+//  
 //
-//  Created by gabosarmiento on 27/11/15.
-//  Copyright (c) 2015 gabosarmiento. All rights reserved.
+//  Created by gabosarmiento on 29/11/15.
+//
 //
 
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *beerPercentTextField;
+@property (weak, nonatomic) IBOutlet UISlider *beerCountSlider;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
 
-
+- (void)buttonPressed:(UIButton *)sender;
 @end
-
---
+++ Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  
+//
+//  Created by gabosarmiento on 28/11/15.
+//
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
